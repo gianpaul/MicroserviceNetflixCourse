1e443383c26aec0dbba31bd2dfd73a30dac4dfba
Gian <gianprp@hotmail.com>
2024-11-21 23:24:55 -0500

    feat: move microservice in workspace

--- src/main/java/exirium/pe/SpringCloud/repositories/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package exirium.pe.SpringCloud.repositories;
-
-import exirium.pe.SpringCloud.entities.Product;
-import org.springframework.data.repository.CrudRepository;
-
-public interface ProductRepository extends CrudRepository<Product, Long> {
-
-
-}
